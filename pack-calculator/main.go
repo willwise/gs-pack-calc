7792fcca0c8225ce9bc2b824ed640bdcda0e0720
William Wise <williamwise@Unknown-74-8f-3c-bc-b2-d6.home>
2021-04-30 14:11:10 +0100

    adding in code to handle the order

--- pack-calculator/main.go
+++ pack-calculator/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 //create data structures
 
@@ -12,12 +16,41 @@ func sortArrayDesc(arr []int) []int {
 	return arr
 }
 
-//function to take the quantity input and loop through the array of packs allowed and return the array of packs required to fulfill the order
+// Function to take the quantity input and loop through the array of packs allowed and return the array of packs required to fulfill the order
+func calcOrder(arr []int, quantity int) []int {
+	var returnArr []int
+	// Order the array desc
+	sortedArr := sortArrayDesc(arr)
+	// Loop through array
+	fmt.Println(len(sortedArr))
 
-// Order the array
-// Loop through
-// Check if remaining items is 0
-// Check if the pack size fits into the remaining items
-// If it does work out the amount of times and the remainder
-// If remainder is > 0 and it is the last one add another pack
-// Else set remainder to the mod
+	// If the quantity is between the final 2 choose the larger number to prevent extra packs
+	if quantity > sortedArr[len(sortedArr)-1] && quantity < sortedArr[len(sortedArr)-2] {
+		returnArr = append(returnArr, sortedArr[len(sortedArr)-2])
+		return returnArr
+	}
+
+	for i := 0; i < len(sortedArr); i++ {
+		// Check if remaining items is 0
+		if quantity == 0 {
+			return returnArr
+		}
+		// Check if the pack size fits into the remaining items
+		if quantity/sortedArr[i] >= 1 {
+			// If it does work out the amount of times
+			numOfPacks := quantity / sortedArr[i]
+			for a := 0; a < numOfPacks; a++ {
+				returnArr = append(returnArr, sortedArr[i])
+			}
+			// Work out how much (if any) the remainder is
+			quantity = int(math.Mod(float64(quantity), float64(sortedArr[i])))
+		}
+		// If remainder is > 0 and it is the last one add another pack
+		if i == len(sortedArr)-1 && quantity > 0 {
+			returnArr = append(returnArr, sortedArr[i])
+			return returnArr
+		}
+
+	}
+	return returnArr
+}
