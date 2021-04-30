7a7e6438db589ff68eb946d09c586ff872265bff
William Wise <williamwise@Unknown-74-8f-3c-bc-b2-d6.home>
2021-04-30 12:08:42 +0100

    adding comments to plan out

--- pack-calculator/main.go
+++ pack-calculator/main.go
@@ -1,1 +1,17 @@
 package main
+
+//create data structures
+
+//function to handle the request check data and respond
+
+//function to take the array input and order it from biggest to smallest
+
+//function to take the quantity input and loop through the array of packs allowed and return the array of packs required to fulfill the order
+
+// Order the array
+// Loop through
+// Check if remaining items is 0
+// Check if the pack size fits into the remaining items
+// If it does work out the amount of times and the remainder
+// If remainder is > 0 and it is the last one add another pack
+// Else set remainder to the mod
